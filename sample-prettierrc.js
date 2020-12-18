78e30af81044bfbd7e994181a85ca36e12968c65
Mark Schmeiser <mark.schmeiser@konzentrik.de>
2020-12-18 08:41:23 +0100

    add sample files for eslint and prettier

---
+++ sample-prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: false,
+  trailingComma: "all",
+  singleQuote: true,
+  printWidth: 120,
+  tabWidth: 4,
+  endOfLine: "lf",
+  eslintIntegration: false,
+  tslintIntegration: true,
+};
